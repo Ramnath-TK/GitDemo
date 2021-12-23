d0d243b0549698ed08de4e66b18756874c5ee491
Ramnath <ramnath.tk@gmail.com>
2021-12-23 00:00:57 +0000

    FourTest added

---
+++ src/test/java/mp/ms/FourTest.java
@@ -0,0 +1,12 @@
+package mp.ms;
+
+import org.testng.annotations.Test;
+
+public class FourTest {
+
+	@Test
+	public void method4()
+	{
+System.out.println("FourTest class file newly added in master by GitDemo");
+	}
+}
